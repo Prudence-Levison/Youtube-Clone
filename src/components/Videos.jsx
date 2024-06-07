aee0b19d1613752e8dd6d340d1cb6e86b936c448
Prudence-Levison <uzoekwelevisoncgmail.com>
2024-06-07 21:56:02 +0100

    working on a youtube clone

---
+++ src/components/Videos.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+const Videos = ({videos}) =>{
+      
+    return(
+        <stack direction='row' flexWrap="wrap" justifyContent="start">
+        </stack>
+    )
+}
+export default Videos;
