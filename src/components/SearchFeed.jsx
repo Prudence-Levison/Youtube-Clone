8ca6c6c8a2536a213ffe289db6174db39eb7de75
Prudence-Levison <uzoekwelevisoncgmail.com>
2024-07-01 10:53:55 +0100

    Youtube app completed

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import { useState, useEffect}from "react";
+import { Box , Typography} from "@mui/material";
+import {Videos} from "./";
+import { fetchFROMAPI } from "../utils/fetchFROMAPI";
+import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
+  const {searchTerm} = useParams() ;
+  const [videos, setVideos] = useState([]);
+  useEffect ( () => {
+  fetchFROMAPI(`search?part=snippet&q=${searchTerm}`)
+  .then((data) => setVideos(data.items))
+  }, [searchTerm])
     return(
-        <div>SearchFeed</div>
+        <Box p={2}  sx={{ overflowY:"auto", height:'90vh', flex: 2}}>
+          <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color:'white'}}>
+             Search Result For: <span style={{ color : '#F31503'}}>{searchTerm}</span> Videos</Typography>
+          <Videos videos={videos} />
+        </Box>
+        
     )
 }
  export default SearchFeed;
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { VideoCard, ChannelCard } from './'
 import { Stack, Box } from "@mui/material";
-const Videos = ({videos}) =>{
-      
+const Videos = ({videos, direction}) =>{
+    if (!videos?.length) return 'loading ...' 
     return(
-        <stack direction='row' flexWrap="wrap" justifyContent="start">
-        </stack>
+        <Stack direction={ direction ||'row'} flexWrap="wrap" justifyContent="start" gap={2}>
+           
+
+            {videos.map((item, idx) =>(
+
+                <Box key={idx}> 
+                {item.id.videoId && <VideoCard   video={item}/>}
+                {item.id.channelId && <ChannelCard   channelDetail={item}/>}
+                </Box>
+            ))}
+        </Stack>
     )
 }
 export default Videos;
